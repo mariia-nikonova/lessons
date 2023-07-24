721b7f9dd41b055ac4645878c12477a719738447
Mariya Nikonova <mariia-nikonova@mail.ru>
2023-07-24 19:43:53 +0300

    [release] Add result ro task 2

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿﻿Console.Write("Введите первое число: ");
+int numb1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int numb2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число: ");
+int numb3 = int.Parse(Console.ReadLine()!);
+
+if ((numb1 > numb2) && (numb1 > numb3)) Console.WriteLine($"{numb1}");
+if ((numb1 < numb2) && (numb2 > numb3)) Console.WriteLine($"{numb2}");
+if ((numb1 < numb3) && (numb2 < numb3)) Console.WriteLine($"{numb3}");
