36c88beae8840f50858be3f6c76020aee5759ad6
Mariya Nikonova <mariia-nikonova@mail.ru>
2023-07-24 19:55:11 +0300

    [Add] Add feature say hello.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿﻿Console.Write("Введите первое число: ");
+﻿Console.Write("Введите первое число: ");
 int numb1 = int.Parse(Console.ReadLine()!);
 Console.Write("Введите второе число: ");
 int numb2 = int.Parse(Console.ReadLine()!);
@@ -8,3 +8,5 @@ int numb3 = int.Parse(Console.ReadLine()!);
 if ((numb1 > numb2) && (numb1 > numb3)) Console.WriteLine($"{numb1}");
 if ((numb1 < numb2) && (numb2 > numb3)) Console.WriteLine($"{numb2}");
 if ((numb1 < numb3) && (numb2 < numb3)) Console.WriteLine($"{numb3}");
+
+Console.WriteLine("Hello dude");
